1330e09a26fe90bbe9f5466ce0be5a66ad93745c
CHARAN TEJA <charan.teja@improve10x.com>
2023-02-02 00:31:39 +0530

    I added datePicker in in xml file and i wrote code for it.

--- app/src/main/java/com/improve10x/uiwidgetsone/UiWidgetsActivity.java
+++ app/src/main/java/com/improve10x/uiwidgetsone/UiWidgetsActivity.java
@@ -3,12 +3,82 @@ package com.improve10x.uiwidgetsone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class UiWidgetsActivity extends AppCompatActivity {
+    Button submitBtn;
+    Button ratingBarBtn;
+    Button seekBarBtn;
+    Button datePickerBtn;
+    TextView progressTxt;
+    SeekBar musicSb;
+    RatingBar ratingBarRb;
+    DatePicker calenderDp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui_widgets);
+        handleInitin();
+        handleSubmitBtn();
+        handleSeekBarBtn();
+        handleRatingBarBtn();
+        handleDatePickerBtn();
+
+    }
+
+    private void handleInitin() {
+        seekBarBtn = findViewById(R.id.seek_bar_btn);
+        progressTxt = findViewById(R.id.progress_txt);
+        submitBtn = findViewById(R.id.submit_btn);
+        musicSb = findViewById(R.id.music_sb);
+        ratingBarBtn = findViewById(R.id.ratingbar_btn);
+        ratingBarRb = findViewById(R.id.ratingbar_rb);
+        datePickerBtn = findViewById(R.id.datepicker_btn);
+        calenderDp = findViewById(R.id.calender_dp);
+    }
+
+    public void handleSeekBarBtn() {
+        seekBarBtn.setOnClickListener(view -> {
+            musicSb.setVisibility(View.VISIBLE);
+            ratingBarRb.setVisibility(View.GONE);
+            calenderDp.setVisibility(View.GONE);
+        });
+    }
+
+    public void handleRatingBarBtn() {
+        ratingBarBtn.setOnClickListener(v -> {
+            ratingBarRb.setVisibility(View.VISIBLE);
+            musicSb.setVisibility(View.GONE);
+            calenderDp.setVisibility(View.GONE);
+        });
+    }
+
+    public void handleDatePickerBtn() {
+        datePickerBtn.setOnClickListener(v -> {
+            calenderDp.setVisibility(View.VISIBLE);
+            ratingBarRb.setVisibility(View.GONE);
+            musicSb.setVisibility(View.GONE);
+        });
+    }
+
+    private void handleSubmitBtn() {
+        submitBtn.setOnClickListener(view -> {
+            int progress = musicSb.getProgress();
+            progressTxt.setText(progress + " ");
+            String rating = String.valueOf(ratingBarRb.getRating());
+            progressTxt.setText(rating);
+            String year = String.valueOf(calenderDp.getYear());
+            String month = String.valueOf(calenderDp.getMonth());
+            String date = String.valueOf(calenderDp.getDayOfMonth());
+            progressTxt.setText(date + " / " + month + " / " + year);
+
+        });
     }
 }
