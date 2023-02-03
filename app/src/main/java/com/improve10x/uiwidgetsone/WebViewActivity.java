4216aa4ab5f2fae6d9dcbf90d14849d7e9a714c8
CHARAN TEJA <charan.teja@improve10x.com>
2023-02-03 11:31:19 +0530

    give internet permissions in AndroidManifest.xml

--- app/src/main/java/com/improve10x/uiwidgetsone/WebViewActivity.java
+++ app/src/main/java/com/improve10x/uiwidgetsone/WebViewActivity.java
@@ -3,12 +3,16 @@ package com.improve10x.uiwidgetsone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
-public class WebView extends AppCompatActivity {
+public class WebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        WebView myWebView = findViewById(R.id.web_view_wv);
+        myWebView.loadUrl("https://improve10x.com/home/index.html");
     }
 }
