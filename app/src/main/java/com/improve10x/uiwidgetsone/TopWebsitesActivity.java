36ff7fb39beee61dfe3b611ba13079932e6fb9b5
CHARAN TEJA <charan.teja@improve10x.com>
2023-02-03 12:30:52 +0530

    I Created TopWebsitesActivity in UiWidgetsOne project

---
+++ app/src/main/java/com/improve10x/uiwidgetsone/TopWebsitesActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.uiwidgetsone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopWebsitesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_websites);
+    }
+}
